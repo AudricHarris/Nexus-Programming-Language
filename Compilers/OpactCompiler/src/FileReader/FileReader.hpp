5c14ca34ee063540cb11f5be57e142024538de7e
Audric Harris <audricharrismail@gmail.com>
2026-09-11 16:25:21 +0200

    Added Documentation for ReadFile

--- Compilers/OpactCompiler/src/FileReader/FileReader.hpp
+++ Compilers/OpactCompiler/src/FileReader/FileReader.hpp
@@ -1,9 +1,14 @@
+/**
+ * @file FileReader.hpp
+ * @brief this Handles reading a file this used to have a bigger role. Now it only reads a file at given name
+ * */
 #ifndef File_Reader
 #define File_Reader
 
 #include <optional>
 #include <string>
 
+/** @brief reads the file that you put as in params and returns a string if it exists*/
 std::optional<std::string> readFile(const char *name);
 
 #endif
